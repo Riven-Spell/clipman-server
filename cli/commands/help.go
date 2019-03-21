15e128dadd5a747d8518e8e288315fe5e95f2278
Adele Lynn <virepri2k@gmail.com>
2019-03-20 23:44:26 -0700

    Individual help commands

--- cli/commands/help.go
+++ cli/commands/help.go
@@ -3,7 +3,15 @@ package commands
 import "fmt"
 
 func help(args []string) {
-	for k, v := range HelpList {
-		fmt.Println(k + ":\n" + v + "\n")
+	if len(args) >= 1 {
+		if v, ok := HelpList[args[0]]; ok {
+			fmt.Println(args[0] + ":\n" + v + "\n")
+		} else {
+			fmt.Println(args[0], "is not a valid command.")
+		}
+	} else {
+		for k, v := range HelpList {
+			fmt.Println(k + ":\n" + v + "\n")
+		}
 	}
 }

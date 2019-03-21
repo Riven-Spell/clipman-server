9fdd00760e96d4647503ea87034f6a42873ac2d0
Adele Lynn <virepri2k@gmail.com>
2019-03-20 18:12:34 -0700

    Loading & saving configs is now available.

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/virepri/clipman-server/cli"
 	"github.com/virepri/clipman-server/listener"
 	"github.com/virepri/clipman-server/shared"
+	"os/user"
 )
 
 func main() {
 	shared.ActiveServices.Add(1)
+
+	if u, err := user.Current(); err == nil {
+		shared.ConfigLocation = u.HomeDir + "/.config/clipman-server.cfg"
+	} else {
+		fmt.Println("Can't get current user:")
+		fmt.Println(err.Error())
+		return
+	}
+
+	shared.LoadCFG()
 
 	go cli.StartCLI()
 	go listener.StartListener()
--- shared/config.go
+++ shared/config.go
@@ -1,5 +1,50 @@
 package shared
 
-//TODO: load and save configs.
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 var BufferSize = 1024
 var BindTo = ":7606"
+var ConfigLocation = ""
+
+type cfg struct {
+	buffer int
+	bind string
+}
+
+func SaveCFG() {
+	c := cfg {BufferSize, BindTo}
+
+	buff, _ := json.Marshal(c)
+	if f, err := os.OpenFile(ConfigLocation, os.O_CREATE | os.O_RDWR, 0554); err == nil {
+		if _, err := f.Write(buff); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
+func LoadCFG() {
+	var c cfg
+
+	if fi, err := os.Stat(ConfigLocation); err == nil {
+		buff := make([]byte, fi.Size())
+		if f, err := os.Open(ConfigLocation); err == nil {
+			if _, err := f.Read(buff); err == nil {
+				if err := json.Unmarshal(buff, &c); err == nil {
+					BufferSize = c.buffer
+					BindTo = c.bind
+					return
+				}
+			}
+		}
+	}
+
+	fmt.Println("Can't find a config. Creating a default config.")
+	SaveCFG()
+}
